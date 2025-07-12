f4f3faea1a606bd927bd5e17daab65eeef2a1dd5
Hamza Kamelen <hamzakamelen.dev@gmail.com>
2025-07-13 00:03:49 +0500

    COde

---
+++ Objects/app.js
@@ -0,0 +1,77 @@
+// Intitialize Object
+// var plan1 = {}
+
+// var abc = "hdasdjas"
+// var abc = {}
+
+// Assign Properties to Object
+// var plan1 = {
+    // Name : Value,
+// }
+
+
+// ----------------------
+
+// var plan1 = {
+//     name: "Basic",
+//     price: 3.99,
+//     // space: 100,
+//     // transfer: 1000,
+//     // pages: 10
+// }
+
+// console.log(plan1);
+// plan1.price = 200
+// console.log(plan1);
+// plan1.newValue = "ABCD"
+// console.log(plan1);
+// plan1.Abcd = ["A","B","C","D"]
+// console.log(plan1);
+// var second = plan1.Abcd[2]
+// console.log(second);
+
+// delete plan1.Abcd
+// console.log(plan1);
+
+
+
+// var plan1 = {
+//     name: "Basic",
+//     price: 3.99,
+//     space: 100,
+//     transfer: 1000,
+//     pages: 10,
+//     bestPrice: function calculate(){
+//         return this.price + 50 
+//     }
+// }
+
+// console.log(plan1.bestPrice());
+
+
+// function Plan(name,price,abc){
+//     this.a = name
+//     this.b = price
+//     this.calculate = abc
+// }
+
+// var plan1 = new Plan("ABCD",200,"ABCD")
+// var plan2 = new Plan("XYZ",400,"ABCD")
+// var plan3 = new Plan("ZYX",500,"ABCD")
+// console.log(plan1);
+// console.log(plan2);
+// console.log(plan3);
+
+// plan1.Add = "EYE"
+
+// console.log(plan1);
+// console.log(plan2);
+// console.log(plan3);
+
+// console.log("Add" in plan1);
+// console.log("Add" in plan2);
+
+// console.log(plan1.hasOwnProperty("Add"));
+// console.log(plan2.hasOwnProperty("Add"));
+
+
